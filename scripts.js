05de21656af6a4554aff868187d1f3c32c1b3d32
rockleeqnerock <laurinhapaznunes@gmail.com>
2024-03-01 21:16:43 -0300

    realizando alteração no layout com flexbox

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const Observador = new IntersectionObserver((entries) => {
+    entries.forEach((entry) =>{
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+const elements = document.querySelectorAll('.hidden')
+elements.forEach((elements) => Observador.observe(elements))
+
+/*sistema que observa o que está na tela e verifica se precisa ou não 
+substituir o hidden pelo show*/
