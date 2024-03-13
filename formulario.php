787ed22086d4fd14be083bbf6d8521a2296b2efd
rockleeqnerock <laurinhapaznunes@gmail.com>
2024-03-12 21:34:29 -0300

    criando um formulário para adicionar notícias

---
+++ formulario.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="style.css">
+
+
+
+<form action="formulario.php" method="post" class="form">
+<label for="title">Titulo</label>
+<input type="text" id="title" name="nome-titulo" class="input">
+
+<label for="subtitle">Subtítulo</label>
+<input type="text" id="subtitle" name="name-sub" class="input">
+<br>
+
+<label for="autor">Autor</label>
+<input type="text" id="autor" name="name-autor" class="input">
+
+<label for="image">Imagem</label>
+<input type="text" id="image" name="imagem" class="input">
+<br>
+<br>
+<br>
+
+<label for="notice" >Notícia:</label>
+<br>
+
+<textarea name="noticia" id="image" cols="60" rows="30" class="input2"></textarea>
+<br>
+
+<input type="submit" value="Enviar" class="enviar">
+
+</form>
